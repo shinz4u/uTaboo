371da4e09041a97fdf7c3ca1910d1392c1f0f864
Vishnu Prathish <vishnu.prathish@gmail.com>
2014-04-30 00:17:32 -0400

    class design

--- uTaboo.py
+++ uTaboo.py
@@ -1,12 +1,42 @@
 class uTaboo:
 
-	def __init__:
-		pass
+    def __init__:
+        pass
 
-	def pickWord(file):
-		pass
+    def pickWord(file):
+        return pickedWord
+        pass
 
+    def getGoogledURLS(pickedWord):
+        return urls
+        pass
+        
+        
+    def fetchHTML(url):
+        return htmlData
+        pass
+        
+    def getAllHTMS(urls):
+        #use fetchHTML(url) to obtain a concatenated list of htmldata
+        pass
+        
+    def html2Text(htmlData):
+        return textData
+        pass
+        
+   
+    # Second Module
 
-	
-
-
+    def filterStopWords(listOfWords):
+        pass
+        
+    def getRankedList(filteredListofWords):
+        pass
+        
+    def fetchTop6(rankedListOfWords):
+        pass
+        
+    def getTabooWords(word,top6):
+        #return a dictionary with word as key and a list of 6 words as value
+        pass
+       
